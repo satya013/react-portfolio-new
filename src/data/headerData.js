2f352ae07066d49e62a18939a8b402dab0a2cd3f
Satyabrata <deccor817@gmail.com>
2024-08-23 13:49:26 +0530

    updated resume pdf

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,11 @@
-import resume from '../assets/pdf/Satyabrata_Mohapatra_ASE_.pdf'
+import resume from "../assets/pdf/Satyabrata_Mohapatra_ASE_.pdf";
 
 export const headerData = {
-    name: 'Satyabrata Mohapatra',
-    title: "Web Developer",
-    desciption:"Passionate Frontend Developer specializing in creating intuitive and dynamic user experiences. Leveraging modern React technologies to build responsive and efficient web applications."
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
-    resumePdf: resume
-}
+  name: "Satyabrata Mohapatra",
+  title: "Web Developer",
+  desciption:
+    "Passionate Frontend Developer specializing in creating intuitive and dynamic user experiences. Leveraging modern React technologies to build responsive and efficient web applications.",
+  image:
+    "https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png",
+  resumePdf: resume,
+};
